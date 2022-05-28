7a259d04054fae86dc24562954162717eecac88f
pr0kk <105685714+pr0kk@users.noreply.github.com>
2022-05-28 09:49:45 +0900

    Implement Signup function + signup.html + UserMapper + UserService + SignupController

--- starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/controller/SignupController.java
+++ starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/controller/SignupController.java
@@ -1,14 +1,49 @@
 package com.udacity.jwdnd.course1.cloudstorage.controller;
 
+import com.udacity.jwdnd.course1.cloudstorage.model.User;
+import com.udacity.jwdnd.course1.cloudstorage.services.UserService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/signup")
 public class SignupController {
+
+    private UserService userService;
+
+    public SignupController(UserService userService) {
+        this.userService = userService;
+    }
+
     @GetMapping()
     public String getSignupPage() {
+        return "signup";
+    }
+
+    @PostMapping()
+    public String signup(@ModelAttribute User user, Model model){
+        String signupErrorMessage = null;
+
+        if (!userService.isUsernameAvailable(user.getUsername())) {
+            signupErrorMessage = "The username already exists.";
+        }
+
+        if (signupErrorMessage == null) {
+            int userInserted = userService.createUser(user);
+            if (userInserted < 0) {
+                signupErrorMessage = "There was an error signing you up. Please try again.";
+            }
+        }
+
+        if (signupErrorMessage == null) {
+            model.addAttribute("signupSuccess", true);
+        } else {
+            model.addAttribute("signupError", signupErrorMessage);
+        }
         return "signup";
     }
 }
